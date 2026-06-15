e3fe48079e6cd6c366c19774fdecc5990596c50a
Jack‘s <jacks@Ventistage.local>
2026-06-15 12:41:23 +0800

    Initial commit: StreamCap fork for Mac live recorder

---
+++ app/core/media/ffmpeg_builders/video/__init__.py
@@ -0,0 +1,6 @@
+from .flv import FLVCommandBuilder
+from .mkv import MKVCommandBuilder
+from .mov import MOVCommandBuilder
+from .mp4 import MP4CommandBuilder
+from .nut import NUTCommandBuilder
+from .ts import TSCommandBuilder
